4ab15c1e5e5a5ea77f8e44bec8560347e411290a
Alec Ortega <alecortega@live.com>
2017-07-03 18:26:53 -0400

    Make global require

--- client/scripts/build.js
+++ client/scripts/build.js
@@ -1,5 +1,6 @@
 'use strict';
 
+global.__base = __dirname + '/client';
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
