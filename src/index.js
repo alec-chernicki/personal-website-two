bf7306a0baa83f6ec754fdfcb5be6ea320a12f14
Alec Ortega <alecortega@live.com>
2017-07-15 15:37:33 -0400

    Add service worker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './index.css';
 import { render } from 'react-snapshot';
-
 import {Router, browserHistory} from 'react-router';
 import routes from './config/routes';
+import registerServiceWorker from './registerServiceWorker';
+
+registerServiceWorker.register();
 
 render(
   <Router history={browserHistory}>
